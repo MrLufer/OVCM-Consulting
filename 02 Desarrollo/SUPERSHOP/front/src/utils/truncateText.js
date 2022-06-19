8a5de678e708b8fda2e055705cdf618a71b72d49
Luis Fernando Apaza Gutierrez <lapaza@timejobs.work>
2022-06-18 23:39:26 -0500

    feature admin front

---
+++ 02 Desarrollo/SUPERSHOP/front/src/utils/truncateText.js
@@ -0,0 +1,8 @@
+export function truncateText(text) {
+    if (text.length > 15) {
+      const truncatedText = text.substr(0, 15);
+      return truncatedText + "...";
+    }
+    return text;
+  }
+  
