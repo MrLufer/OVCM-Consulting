1ec6cd6425928aea5884e0960629168eca1b2669
Luis Fernando Apaza Gutierrez <luferapaza@gmail.com>
2022-08-19 14:27:47 -0500

    build prod

---
+++ 02Desarrollo/SUPERSHOP/front/build/precache-manifest.aa213ff05f3650d42e42bb43e1e1e375.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e472b6c0a583f395c4415e2d1c5aed2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4279ce3503c2221da75",
+    "url": "/static/css/main.8144939e.chunk.css"
+  },
+  {
+    "revision": "eacc43a0fb5917b98ad1",
+    "url": "/static/js/2.fbd35cb4.chunk.js"
+  },
+  {
+    "revision": "a4279ce3503c2221da75",
+    "url": "/static/js/main.978e8722.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
